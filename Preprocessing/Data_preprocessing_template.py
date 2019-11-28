45e0fcd9fc529c1ed12a244b70a46563afed72ba
Abhishek Kamal <kamalabhishek20@gmail.com>
2019-11-28 22:17:02 +0530

    Linear Regression

--- Preprocessing/Data_preprocessing_template.py
+++ Preprocessing/Data_preprocessing_template.py
@@ -30,10 +30,23 @@ X[:,1:3] = imputer.transform(X[:,1:3])
 
 # 4. Encoding Categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-#labelencoder_X = LabelEncoder()
-#X[:,0]=labelencoder_X.fit_transform(X[:,0])
+labelencoder_X = LabelEncoder()
+X[:,0]=labelencoder_X.fit_transform(X[:,0])
 onehotencoder = OneHotEncoder(categorical_features=[0])
 X=onehotencoder.fit_transform(X).toarray()
 labelencoder_y=LabelEncoder()
 Y=labelencoder_y.fit_transform(Y)
+
+
+# 5. Splitting the dataset into testing and training dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random_state=0)
+
+
+# 6. Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
 
